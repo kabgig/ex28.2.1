b287812a24132dbc59d4fccd53b3d9db9392e93a
Ansar Ismagilov <ansar89kab@bk.ru>
2023-07-21 15:05:43 +0800

    done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.lang.reflect.Modifier;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class ArrayListClass = Class.forName("java.util.ArrayList");
+        Class AbstractListClass = Class.forName("java.util.AbstractList");
+        Class CollectionClass = Class.forName("java.util.Collection");
+
+        int arrayListClassModifiers = ArrayListClass.getModifiers();
+        int abstractListClassModifiers = AbstractListClass.getModifiers();
+        int collectionClassModifiers = CollectionClass.getModifiers();
+
+        System.out.println("Array is public: " + Modifier.isPublic(arrayListClassModifiers));
+        System.out.println("Array is protected: " + Modifier.isProtected(arrayListClassModifiers));
+        System.out.println("AbstractList is static: " + Modifier.isStatic(abstractListClassModifiers));
+        System.out.println("AbstractList is abstract: " + Modifier.isAbstract(abstractListClassModifiers));
+        System.out.println("Collection is interface: " + Modifier.isInterface(collectionClassModifiers));
     }
 }
